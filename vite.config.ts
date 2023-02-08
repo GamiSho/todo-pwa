6c6460e3713de22372bccfd8ff71c312f0f1f578
Sho Yasugami <vitamin.sho@gmail.com>
2023-02-08 13:53:56 +0900

    Install new package for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Shasugami Todo App (PWA)',
+        short_name: 'Shodo',
+        description: 'Sample Todo PWA',
+        start_url: '.',
+        display: 'standalone',
+        orientation: 'portrait',
+        theme_color: '#3f51b2',
+        background_color: '#efeff4',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512-mask.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ]
+      }
+    })
+  ],
 })
